4749d6d66b5788077038581c8acce8fea34762e0
raycielo <104550257+raycielo@users.noreply.github.com>
2022-05-31 23:02:15 -0500

    Update caesar.js

--- src/caesar.js
+++ src/caesar.js
@@ -7,7 +7,7 @@ const caesarModule = (function () {
   function caesar(input, shift, encode = true) {
     if (!shift || shift === 0 || shift < -25 || shift > 25) { return false }; // if shift is outside of parameters return false
     const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']; // create a alphabet variable
-    const result = [];  // create a result array variable
+    const result = []; 
     input = input.toLowerCase().split('');  
     
     if (encode === true) { 
