ac3eb283204cde81b92b786550226dd14ad6d3f5
raycielo <104550257+raycielo@users.noreply.github.com>
2022-05-31 23:05:47 -0500

    Update substitution.js

--- src/substitution.js
+++ src/substitution.js
@@ -9,26 +9,26 @@ const substitutionModule = (function () {
     const encryptionKeys = {};
 
     if(!alphabet || alphabet.length < 26 || alphabet.length > 26) return false;
-    //97 - 122  char codes a-z
+ 
     let charCodeCount = 97;
 
-    for(let c = 0; c <= 25; c++){ //fill our object with normal alphabet keys paired with substitution keys
-      if(Object.values(encryptionKeys).includes(alphabet[c])){ return false; } //if a value in encryptionKeys already exists (is alphabet[c])
+    for(let c = 0; c <= 25; c++){ 
+      if(Object.values(encryptionKeys).includes(alphabet[c])){ return false; } 
       else{
-        encryptionKeys[String.fromCharCode(charCodeCount)] = alphabet[c]; //set each key to a normal alphabet letter, value to a new alphabet char
+        encryptionKeys[String.fromCharCode(charCodeCount)] = alphabet[c]; 
         charCodeCount++;
       }
     }
 
     const buildEncryption = input.toLowerCase().split('');
 
-    { //encode or decode here
-		  return buildEncryption.map(letter => {  //iterate through each letter in our input
-		  	for(let plainLetter in encryptionKeys){  //iterate through each object in encryptionKeys
-		  		let substitutionLetter = encryptionKeys[plainLetter]; //set our keyvalue to substitutionLetter
-          if(letter == " ") return " "; //keep spaces
-		  		if(encode && letter == plainLetter) return substitutionLetter; //encode - if our input letter = key's string of plain letter, return the value of subsitutionLetter
-          if(!encode && letter == substitutionLetter) return plainLetter; //decode - if our input letter equals a substitution letter, return the plain letter
+    { 
+		  return buildEncryption.map(letter => {  
+		  	for(let plainLetter in encryptionKeys){  
+		  		let substitutionLetter = encryptionKeys[plainLetter]; 
+          if(letter == " ") return " "; 
+		  		if(encode && letter == plainLetter) return substitutionLetter; 
+          if(!encode && letter == substitutionLetter) return plainLetter; 
       }
 		}).join('');
     }
